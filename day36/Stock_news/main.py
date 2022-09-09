77f38a085db36e3a01432efcb27cda58413c638b
Nataliya Kozulina <natasha.pechenka@gmail.com>
2022-09-09 15:26:08 +0600

    pixels

--- day36/Stock_news/main.py
+++ day36/Stock_news/main.py
@@ -5,45 +5,39 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_API_KEY = "QZ3HPQDTWMIF9GYZ"
+NEWS_API_KEY = "c5b1aed00f8f4d668c0044e4c4707841"
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY
+}
+stock_response = requests.get(STOCK_ENDPOINT, params=stock_params).json()
+stock_data = stock_response["Time Series (Daily)"]
+data_list = [value for (key, value) in stock_data.items()]
+yesterday = list(stock_response["Time Series (Daily)"])[0]
+day_before_yesterday = list(stock_response["Time Series (Daily)"])[1]
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+print(yesterday)
+yesterday_close = float(stock_response["Time Series (Daily)"][yesterday]["4. close"])
+day_before_yesterday_close = float(stock_response["Time Series (Daily)"][day_before_yesterday]["4. close"])
+difference = abs(yesterday_close - day_before_yesterday_close)
+percentage = round(difference*100/yesterday_close, 2)
+print(f"""yesterday_close = {yesterday_close}
+day_before_yesterday_close = {day_before_yesterday_close}
+difference = {difference}
+percentage = {percentage}%""")
 
-#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+news_params = {
+    "q": COMPANY_NAME,
+    "language": "ru",
+    "sortBy": "relevance",
+    "apiKey": NEWS_API_KEY
+}
+news_response = requests.get(NEWS_ENDPOINT, params=news_params).json()["articles"][:3]
 
-#TODO 2. - Get the day before yesterday's closing stock price
-
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
-
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
-
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-#TODO 9. - Send each article as a separate message via Twilio. 
-
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+for news in news_response:
+    print(f"""
+    Title: {news["title"]}
+    {news["description"]}
+    link: {news["url"]}
+    """)
---
+++ day37/API_POST/main.py
@@ -0,0 +1,61 @@
+import requests
+import datetime as dt
+USERNAME = "nutamy"
+TOKEN = "Z26SgOItjzmtaR)PX%R"
+GRAPH_ID = "pythonyellow"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Python coding",
+    "unit": "commit",
+    "type": "int",
+    "color": "ichou"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+now = dt.datetime(year=2022, month=7, day=7)
+today = now.strftime("%Y%m%d")
+
+pixel_data = {
+    "date": today,
+    "quantity": "10"
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4"
+}
+
+## PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+## DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
