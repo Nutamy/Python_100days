ce88674e5b8e0432207ee339e7f7f5cd5e12805e
Nataliya Kozulina <natasha.pechenka@gmail.com>
2022-09-14 12:49:36 +0600

    email success and bot start

---
+++ day45/BeautifulSoup/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+<
+website.html,2\0\20e282a78874a25d0ff104e94a6b6b2c64e32891
+:
+
+movies.txt,b\5\b578148a55535a2cf04234b4cbff58c4375308db
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+7
+main.py,f\1\f1bdda93d9a278e358509d498e17d97764c1fb29
---
+++ day46/TimeMachine/main.py
@@ -0,0 +1,68 @@
+from turtledemo.minimal_hanoi import play
+
+import requests
+from bs4 import BeautifulSoup
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+import smtplib
+
+
+# CONSTANTS
+ID = os.environ["CLIENT_ID"]
+SECRET = os.environ["CLIENT_SECRET"]
+MY_EMAIL = os.environ["EMAIL"]
+RECIPIENT = os.environ["RECIPIENT"]
+MY_PASSWORD = os.environ["PASSWORD"]
+
+
+# LOGIN SPOTIFY
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=ID,
+                                                           client_secret=SECRET))
+
+
+# GET DATE FOR SEARCHING TOP 5 SONGS
+date = input("Which year do you want to travel? "
+             "Type thr date in this format YYYY-MM-DD: ")
+url = f"https://www.billboard.com/charts/hot-100/{date}/"
+
+
+# REQUEST TO CHARTS
+response = requests.post(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+# CREATING LIST OF TOP 5 SONGS
+top = soup.find_all(name="h3", id="title-of-a-story", class_="a-truncate-ellipsis")
+top_5 = [tag.getText().strip() for tag in top][:5]
+
+
+#  GET LINKS FROM SPOTIFY
+playlist = []
+for song in top_5:
+    playlist.append(sp.search(q=song, limit=1))
+
+links = []
+for song in playlist:
+    link = song['tracks']['items'][0]['album']['artists'][0]['external_urls']['spotify']
+    links.append(link)
+
+def sewing(song, link):
+    return f"{song}: {link}\n"
+
+msg = map(sewing, top_5, links)
+massage_body = ""
+for row in msg:
+    massage_body += row
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASSWORD)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=RECIPIENT,
+        msg=f"Subject:Let's travel in {date}\n\n"
+            f"TOP 5:\n\n {massage_body}")
+    print("done")
+
+
+
