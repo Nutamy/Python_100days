d7b34820ccc587fdbd61add17b408be90d1e9129
Nataliya Kozulina <natasha.pechenka@gmail.com>
2022-09-20 22:29:50 +0600

    done flask

---
+++ ZTM/web/first/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+F
+templates/streams.html,3\3\33ae98f408ab006f0e1c6fba8e1c7384fa0dd5bf
+F
+templates/profile.html,7\f\7f1863afc75308f4b2ba18256c163b6f3c56c5d1
+F
+templates/details.html,e\e\eeb517c2cfa4303c8e689a83f3bb9d352aa04563
+6
+app.py,1\3\13cce7fd076299c81b4986166f3d822791c9490e
+D
+templates/index.html,f\6\f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
+E
+templates/browse.html,7\4\749af96da8c74bcc9bdad4ef1a3564e63998dc9f
--- day57/Jinja/app.py
+++ day57/Jinja/app.py
@@ -4,7 +4,9 @@ import requests
 
 app = Flask(__name__)
 year = dt.datetime.now().year
-
+url_blog = "https://api.npoint.io/0a50e5219852c16cb6cb"
+response_blog = requests.get(url_blog)
+all_posts = response_blog.json()
 
 @app.route("/")
 def hello_world():
@@ -24,9 +26,15 @@ def guess(cur_name):
     return render_template("name.html", year=year, gender=gender, age=age, name=name)
 
 @app.route("/blog")
-def blog():
-    url_blog = "https://api.npoint.io/0a50e5219852c16cb6cb"
-    response_blog = requests.get(url_blog)
-    all_posts = response_blog.json()
+def get_blog():
     return render_template("blog.html", year=year, posts=all_posts)
+
+@app.route("/post/<int:idx>")
+def get_post(idx):
+    requested_post = None
+    for blog_post in all_posts:
+        if blog_post["id"] == idx:
+            requested_post = blog_post
+            break
+    return render_template("post.html", year=year, post=requested_post)
 
